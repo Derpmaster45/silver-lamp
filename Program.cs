6bbe0fc1b7f6ddc918fd2f284fb3a2468fb7c498
Derpmaster45 <jmsteltz04@gmail.com>
2024-05-06 13:09:51 -0500

    Hello testing branch

--- Program.cs
+++ Program.cs
@@ -27,6 +27,11 @@ namespace DH4
                 switchinput="";
                 
             }
+            void BattleSystem(Character[] PlayerParty, Enemy[] enemy)
+            {
+                //onsole.BackgroundColor = Color.White;
+                //ResetAndClear();
+            }
            string MainMenuOption="";
            while(MainMenuOption=="")
            {
@@ -49,7 +54,41 @@ namespace DH4
                 PromptedClearScreen();
                 Console.WriteLine("Angel: You leave me no choice.\n The angel raises his hand with a fire burning on his palm.\n Town Elder: WAIT! We will take you to them! Please don't harm the village.\n");
                 Console.WriteLine("The villagers take the angel, to a house on lake askela, on the outskrits of the village.\n");
-                Console.WriteLine("Angel: Dark swordsman, you, and the mage have been given up by the villagers.\n");           
+                Console.WriteLine("Angel: Dark swordsman, you, and the mage have been given up by the villagers.\n");
+                // party setup for prologue     
+                Character[] MageAndSwordsman=new Character[2];
+                MageAndSwordsman[0].PlayerName="Mage";
+                MageAndSwordsman[1].PlayerName="Swordsman";
+                MageAndSwordsman[0].PlayerLevel=10; 
+                MageAndSwordsman[1].PlayerLevel=10;
+                MageAndSwordsman[0].AttackPoints=40;
+                MageAndSwordsman[1].AttackPoints=40;
+                MageAndSwordsman[0].PlayerDefensePoints=2;
+                MageAndSwordsman[1].PlayerDefensePoints=2;
+                MageAndSwordsman[0].PlayerManaPoints=0;
+                MageAndSwordsman[1].PlayerManaPoints=500;
+                MageAndSwordsman[0].PlayerHeath=1000;
+                MageAndSwordsman[1].PlayerHeath=950;
+                // party setup complete
+                Console.WriteLine("The door swings open, out steps a man who is average in build, and not very intimidating.\n");
+                Console.WriteLine("Angel: Ha ha ha, you're the dark swordsman? \nFrom the stories I have heard, I expected you to be scarier.\n");
+                Console.WriteLine("Unphazed by the comments of the angel, THe dark swordsman, takes a deep breath.\n Dark Swordsman: Who might you be?\n");
+                Console.WriteLine("I am a follower of the Great Spirit. The god that you 'killed' with that mage, speaking of which where is she.\n  Dark swordsman:She's busy. \n");
+                Console.WriteLine("Dark Swordsman: Wsat do you want.\n Angel: It's quite simple really, I want to destroy you and the mage.\n");
+                PromptedClearScreen();
+                 Console.WriteLine("If I just take you on, it won't be much fun. It seems like the stories are wrong.\nYou were made out to be this unstoppable force.\n");
+                 Console.WriteLine("Before the dark swordsman can respond a woman steps out from the empty doorway.\n Mage: Whats going on\n");
+                 Console.WriteLine("The angel has come here for a fight, however the angel has gotten the village involved.\n");
+                 Console.WriteLine("Mage: If you want a fight with us, we will give it to you, howeverleave the villagers out of this. \nThey have done nothing to deserve your anger\n");
+                 Console.WriteLine("Angel: They coexsisted with you, they did not try to exspell you from the village.\nThey are as guilty as you, for lettingyou exsist on this plane.\n");
+                 Console.WriteLine("With that last statement, the dark swordsman looks at the mage, she nods, as if they are planning something \n Angel:Enough of this It is time for you to meet you maker");
+                 Console.WriteLine("The mage snaps her fingers and the villagers disapear\n");
+                 Console.WriteLine("The angel looks around in shock, and anger.\n Angel: WHAT HAPPENED!\n Mage: The villagers safety was in jeopardy, we could not allow you to harm innocent bystanders\n Dark swordsman: Now it is time for you to send a message to your master!\n Dark swordsman: As you know, DEAD MEN TELL NO TALES\n");
+                 // angel enemy object creation
+                 Enemy[] AngelEnemy= new Enemy[1];
+                 AngelEnemy[0].EnemyName="Angel";
+                 AngelEnemy[0].EnemyHeath=900;
+                 //BattleSystem(MageAndSwordsman[0],AngelEnemy[0]);
                 break;
                 // quit game case
                 case"2":
