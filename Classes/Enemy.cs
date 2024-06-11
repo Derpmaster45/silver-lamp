952c0df1e2d0d7db067a3fa365ea9db1229ccc1c
Derpmaster45 <jmsteltz04@gmail.com>
2024-06-11 10:36:17 -0500

    Added max manapoint property

--- Classes/Enemy.cs
+++ Classes/Enemy.cs
@@ -9,6 +9,7 @@ namespace DH4.Classes
         public double EnemyDefensePoints{get; set;}
         public double EnemyHealth{get;set;}
         public double EnemyManaPoint{get;set;}
+        public double EnemyMaxManaPoints{get; set;}
         public double CurrentHealthPoints{get;set;}
         public EnemyNames enemyType {get;set;}
         public int ExpValue {get;set;}
