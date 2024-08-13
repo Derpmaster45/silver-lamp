153ddcd621b2b079aa15890fc6702b8935d606a0
Derpmaster45 <jmsteltz04@gmail.com>
2024-08-13 14:30:55 -0500

    making sure build files are up to date

--- obj/Debug/net8.0/dh4.AssemblyInfo.cs
+++ obj/Debug/net8.0/dh4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dh4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf54f4993fffd7e34dc473a6dc7c8ae928c51a5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7fa77ac26d2e6d465f740e129410b019b72b355")]
 [assembly: System.Reflection.AssemblyProductAttribute("dh4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dh4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
