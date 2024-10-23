0b96cb454906c56716fc4226fd1750e24b1626a6
Derpmaster45 <jmsteltz04@gmail.com>
2024-10-22 21:38:24 -0500

    build changes

--- obj/Debug/net8.0/dh4.AssemblyInfo.cs
+++ obj/Debug/net8.0/dh4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dh4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be12a14cef4b230064a8793356c3ee72b0410b00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e5b97956e3d965ad877c4088da1db79885df1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("dh4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dh4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
