c3475b650f3ee03b1b9523bf9017f8a1f3bb216a
Derpmaster45 <jmsteltz04@gmail.com>
2024-10-26 10:40:30 -0500

    tests  have started

--- obj/Debug/net8.0/dh4.AssemblyInfo.cs
+++ obj/Debug/net8.0/dh4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dh4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8206d13901e29de92d00887d97a45b7afa3f098")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5608e753561d4db31ccae75cc6b6fa5fc12e595c")]
 [assembly: System.Reflection.AssemblyProductAttribute("dh4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dh4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
