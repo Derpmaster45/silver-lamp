bafb5e3fcc4fa04b4ff92d1d7ef2f490accfa925
Derpmaster45 <jmsteltz04@gmail.com>
2024-11-04 13:11:03 -0600

    reworked quit game

--- Program.cs
+++ Program.cs
@@ -7,24 +7,48 @@ namespace DH4
     {
         public void BattleSystem(Enemy enemy, Character playercharacter)
         {
-            /**/
+            /*Todo:
+            Change color for player attacks and enemy attacks
+            have a boolen for petrification
+            set parameters for the above state
+            test changes 
+            */
             while(playercharacter.CurrentHealthPoints==0|| enemy.CurrentHealthPoints==0)
             {
                 
             }
         }
-        public void QuitGame()
-        {
-            System.Console.WriteLine("Quittig game in 3 seconds\n");
-            Thread.Sleep(3000);
-            System.Console.WriteLine("GoodBye");
-            Environment.Exit(0);
-        }
+        
         // game code goes here
         public static void Main(string[] args)
         {
             // title screen goes here
+            string TitleScreenOption="";
+            while (TitleScreenOption=="")
+            {
+                System.Console.WriteLine("Demon Hunters 5 New Generation\n 1) New Game \n 2) Quit\n");
+                TitleScreenOption=Console.ReadLine();
+                switch (TitleScreenOption.ToLower())
+                {
+                    // new game option
+                    case "1":
+                    case "new game":
+                    break;
+                    // quit game option
+                    case "2":
+                    case "quit":
+                        QuitGame();
+                    break;
+                }
+            }
 
+        }
+        public static void QuitGame()
+        {
+            System.Console.WriteLine("Quitting game in 3 seconds\n");
+            Thread.Sleep(3000);
+            System.Console.WriteLine("GoodBye");
+            Environment.Exit(0);
         }
     }
 }
