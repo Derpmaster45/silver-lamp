4b3ce4ce1f0fe8c018004c456bc3fb5ae771100f
Derpmaster45 <jmsteltz04@gmail.com>
2024-11-04 12:24:24 -0600

    Started the long rework process

--- obj/Debug/net8.0/dh4.AssemblyInfo.cs
+++ obj/Debug/net8.0/dh4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dh4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f30b58171fe79ac3fa57f2c278523149eb5db16a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c4b3517e54e273123949fa10f2d68a32b2acd52")]
 [assembly: System.Reflection.AssemblyProductAttribute("dh4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dh4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
