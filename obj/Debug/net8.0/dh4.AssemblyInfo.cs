bd9d55d4476ef06e968436ee4a263d3b97625ba9
Derpmaster45 <jmsteltz04@gmail.com>
2025-01-30 11:28:02 -0600

    had to install dotnet 8

--- obj/Debug/net8.0/dh4.AssemblyInfo.cs
+++ obj/Debug/net8.0/dh4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dh4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+368b050ad564dc15dc3310dc335f798b649a87da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fcfef5325f943244ce8c6a2349ca94f4c60e18d")]
 [assembly: System.Reflection.AssemblyProductAttribute("dh4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dh4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
